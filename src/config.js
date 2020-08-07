a327d6132445016c2906d44358190aea0972c946
4ked <max.goeke0@gmail.com>
2020-08-06 22:04:57 -0500

    configuring amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes--file-uploads",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://dlvzgmyrbk.execute-api.us-east-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_csVpvvD7z",
+    APP_CLIENT_ID: "7h9sg6uulgipqs1actaa21ilaf",
+    IDENTITY_POOL_ID: "us-east-2:73b92d79-d7b8-4179-a6ad-7ab15c0a6a24",
+  },
+};
